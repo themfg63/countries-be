7b7fd94c1ab274ea1b19ea3c4e052ccb08049046
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-02-29 16:02:34 +0300

    proje yapısı oluşturuldu

---
+++ src/main/java/com/themfg/countriesbe/CountriesbeApplication.java
@@ -0,0 +1,13 @@
+package com.themfg.countriesbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountriesbeApplication.class, args);
+	}
+
+}
